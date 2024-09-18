c5f5033b100bb0a35d99f8f7c918c6bd0293521a
Cainã Costa <me@cfcosta.com>
2024-09-17 21:43:52 -0300

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,122 @@
+{
+  description = "Hydra control plane";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    cardano-node.url = "github:intersectmbo/cardano-node/9.0.0";
+    hydra.url = "github:cardano-scaling/hydra/0.17.0";
+
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  nixConfig = {
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://cache.iog.io/"
+    ];
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      cardano-node,
+      hydra,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        inherit (pkgs) makeRustPlatform mkShell rust-bin;
+        inherit (pkgs.lib) optionals;
+        inherit (pkgs.stdenv) isDarwin;
+
+        rust = rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        platform = makeRustPlatform {
+          rustc = rust;
+          cargo = rust;
+        };
+      in
+      {
+        packages.default = platform.buildRustPackage {
+          name = "hydra-control-plane";
+          src = ./.;
+          buildInputs =
+            with pkgs;
+            (
+              [
+                pkg-config
+                openssl
+              ]
+              ++ optionals isDarwin [
+                darwin.apple_sdk.frameworks.SystemConfiguration
+              ]
+            );
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+
+            outputHashes = {
+              "pallas-0.29.0" = "sha256-P//R/17kMaqN4JGHFFTMy2gbo7k+xWUaqkF0LFVUxWQ=";
+            };
+          };
+        };
+
+        devShells.default = mkShell {
+          buildInputs =
+            [
+              # Runtime dependencies
+              cardano-node.packages.${system}.cardano-cli
+              hydra.packages.${system}.hydra-node
+
+              # Rust Environment
+              rust
+              pkgs.pkg-config
+              pkgs.openssl
+              pkgs.python312Packages.virtualenvwrapper
+            ]
+            ++ optionals isDarwin [
+              pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+            ];
+
+          shellHook =
+            let
+              lib-path = pkgs.lib.makeLibraryPath [
+                pkgs.libffi
+                pkgs.openssl
+                pkgs.stdenv.cc.cc
+              ];
+            in
+            ''
+              # Augment the dynamic linker path
+              export "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${lib-path}"
+              SOURCE_DATE_EPOCH=$(date +%s)
+
+              if test ! -d .venv; then
+                virtualenv .venv
+              fi
+
+              source ./.venv/bin/activate
+              export PYTHONPATH=`pwd`/.venv/${pkgs.python312.sitePackages}/:$PYTHONPATH
+            '';
+        };
+      }
+    );
+}
