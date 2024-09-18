bfd503acf62ddca940aa34ec1af245830fed9cfc
Cainã Costa <me@cfcosta.com>
2024-09-18 12:31:54 -0300

    chore: setup tracing

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,22 @@ mod net_client;
 mod net_packet;
 mod net_structs;
 
-use net_client::NetClient;
+use tracing::info;
+
+use self::net_client::NetClient;
 
 fn main() {
-    // Initialize the client
+    tracing_subscriber::fmt::init();
+
+    info!("Initializing client");
     let mut client = NetClient::new("Player1".to_string(), false);
     client.init();
 
-    // Run the client
+    info!("Client initialized, starting main loop");
+
     loop {
         client.run();
+
         // Add some delay to prevent busy-waiting
         std::thread::sleep(std::time::Duration::from_millis(10));
     }
